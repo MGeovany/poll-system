60a6570b2cdeaa44f8e5cc60f0d8c4ca2061438a
Marlon Geovany Castro <37640685+MGeovany@users.noreply.github.com>
2024-10-27 21:25:52 -0600

    feat: added put to update polls data

--- src/main/java/com/mgeovany/pollsystem/controllers/PollController.java
+++ src/main/java/com/mgeovany/pollsystem/controllers/PollController.java
@@ -9,6 +9,8 @@ import com.mgeovany.pollsystem.repositories.OptionRepository;
 import com.mgeovany.pollsystem.repositories.PollRepository;
 import com.mgeovany.pollsystem.utils.GenerateRandomUrl;
 
+import jakarta.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,7 +35,7 @@ public class PollController {
         Poll poll = new Poll();
         poll.setTitle(pollRequest.getTitle());
         poll.setUniqueUrl(GenerateRandomUrl.generateUniqueUrl(6));
-    
+
         List<Option> options = new ArrayList<>();
         for (String optionText : pollRequest.getOptions()) {
             Option option = new Option();
@@ -41,47 +43,44 @@ public class PollController {
             option.setPoll(poll);
             options.add(option);
         }
-    
+
         poll.setOptions(options);
         pollRepository.save(poll);
         optionRepository.saveAll(options);
-    
-        
+
         List<String> optionTexts = new ArrayList<>();
         for (Option option : options) {
             optionTexts.add(option.getText());
         }
-    
+
         PollResponse pollResponse = new PollResponse(
-            poll.getId(),
-            poll.getTitle(),
-            poll.getUniqueUrl(),
-            optionTexts
-        );
-    
+                poll.getId(),
+                poll.getTitle(),
+                poll.getUniqueUrl(),
+                optionTexts);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(pollResponse);
     }
-    
+
     @GetMapping
     public ResponseEntity<List<PollResponse>> getAllPolls() {
         List<Poll> polls = pollRepository.findAll();
 
         List<PollResponse> pollResponses = polls.stream()
-            .map(poll -> new PollResponse(
-                poll.getId(),
-                poll.getTitle(),
-                poll.getUniqueUrl(),
-                poll.getOptions().stream()
-                    .map(Option::getText)
-                    .collect(Collectors.toList())
-            ))
-            .collect(Collectors.toList());
+                .map(poll -> new PollResponse(
+                        poll.getId(),
+                        poll.getTitle(),
+                        poll.getUniqueUrl(),
+                        poll.getOptions().stream()
+                                .map(Option::getText)
+                                .collect(Collectors.toList())))
+                .collect(Collectors.toList());
 
         return ResponseEntity.ok(pollResponses);
     }
 
     @GetMapping("/{pollUrl}")
-    public ResponseEntity<PollResponse> getPollById(@PathVariable String pollUrl) {   
+    public ResponseEntity<PollResponse> getPollById(@PathVariable String pollUrl) {
         return pollRepository.findByUniqueUrl(pollUrl)
                 .map(poll -> {
                     List<String> optionTexts = poll.getOptions().stream()
@@ -91,11 +90,46 @@ public class PollController {
                             poll.getId(),
                             poll.getTitle(),
                             poll.getUniqueUrl(),
-                            optionTexts
-                    );
+                            optionTexts);
                     return ResponseEntity.ok(pollResponse);
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
- 
+
+    @PutMapping("/{pollUrl}")
+    @Transactional
+    public ResponseEntity<PollResponse> updatePoll(@PathVariable String pollUrl,
+            @RequestBody PollCreationRequest pollRequest) {
+        return pollRepository.findByUniqueUrl(pollUrl)
+                .map(p -> {
+                    p.setTitle(pollRequest.getTitle());
+
+                    optionRepository.deleteAll(p.getOptions());
+                    p.getOptions().clear();
+
+                    Poll savedPoll = pollRepository.save(p);
+
+                    List<Option> options = new ArrayList<>();
+                    for (String optionText : pollRequest.getOptions()) {
+                        Option option = new Option();
+                        option.setText(optionText);
+                        option.setPoll(savedPoll);
+                        options.add(option);
+                    }
+
+                    optionRepository.saveAll(options);
+                    savedPoll.setOptions(options);
+
+                    savedPoll = pollRepository.save(savedPoll);
+
+                    PollResponse pollResponse = new PollResponse(
+                            savedPoll.getId(),
+                            savedPoll.getTitle(),
+                            savedPoll.getUniqueUrl(),
+                            options.stream().map(Option::getText).collect(Collectors.toList()));
+                    return ResponseEntity.ok(pollResponse);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
